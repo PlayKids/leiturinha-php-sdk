d29d414b0664055eca66691ccd073eb367483d52
jean-rabelo-playkids <jean.rabelo@playkids.com>
2021-01-08 11:02:43 -0300

    [sdk] remove data validation;

--- src/leiturinha/Handler.php
+++ src/leiturinha/Handler.php
@@ -2,20 +2,16 @@
 
 namespace Leiturinha;
 
-use Leiturinha\Traits\ManagesData;
 use Leiturinha\Events\EventFactory;
 
 class Handler
 {
-    use ManagesData;
     private $factory;
 
     public static function receiveData($data)
     {
-        $streamData = (new Handler)->handleData($data);
-
         if (!isset($data) || !empty($data)) {
-            (new EventFactory)->handle($streamData);
+            (new EventFactory)->handle($data);
         }
     }
 
